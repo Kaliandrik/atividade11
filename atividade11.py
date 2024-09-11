b09b03c8dbd02c89ea5a0d845a6079c9d7236929
Kaliandrik <contatokaliandrik@gmail.com>
2024-09-11 15:24:27 -0300

    Verificação de idade

---
+++ atividade11.py
@@ -0,0 +1,25 @@
+# Crie um programa que receba a idade de uma pessoa e classifique-a de acordo com as seguintes faixas etárias:
+# Criança (0-12 anos)
+# Adolescente (13-17 anos)
+# Adulto (18-59 anos)
+# Idoso (60 anos ou mais)
+
+
+
+idade = int(input("Digite sua idade "))
+
+if idade <=12 :
+    print("Você é criança")
+          
+
+
+
+
+if idade >=13 and idade <=17 :
+    print("Você é um adolescente")
+
+if idade >=18 and idade <=59 :
+    print("Você é um adulto")
+
+if idade >=60:
+    print("Você é um idoso")
