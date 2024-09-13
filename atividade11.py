ceca5fa4a6a2cbb458a1d4aec44e99a12b10cabd
Kaliandrik <contatokaliandrik@gmail.com>
2024-09-13 11:43:41 -0300

    Atividade Classificação de idade

--- atividade11.py
+++ atividade11.py
@@ -11,15 +11,11 @@ idade = int(input("Digite sua idade "))
 if idade <=12 :
     print("Você é criança")
           
-
-
-
-
-if idade >=13 and idade <=17 :
+elif idade >=13 and idade <=17 :
     print("Você é um adolescente")
 
-if idade >=18 and idade <=59 :
+elif idade >=18 and idade <=59 :
     print("Você é um adulto")
 
-if idade >=60:
+elif idade >=60:
     print("Você é um idoso")
